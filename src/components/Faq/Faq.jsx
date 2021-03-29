3740d2c46df456286ad87e1065fa289cb92328b0
Shen Chen <schen89@live.com>
2021-03-29 12:10:19 -0400

    optimze mobile.

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -6,11 +6,11 @@ const Faq = () => {
     <div className="container-lg target">
       <div className={`text-center ${styles.icon}`}><img src="/images/site-logo.png" /></div>
       <h2 className="text-center">FAQ</h2>
-      <div className="d-flex justify-content-between">
+      <div className="d-xl-flex justify-content-between">
         <InfoBox style="2" title="How many sites do you monitor?" description="We monitor up to 20+ sites and adding more as we grow." />
         <InfoBox style="2" title="How do I get LS Notify?" description="You may join through restocks, giveaways or group buys." />
       </div>
-      <div className="d-flex justify-content-center">
+      <div className="d-xl-flex justify-content-center">
         <InfoBox style="2" title="Do you guys have 24/7 suupport?" description="Yes, we offer 24/7 support through all times." />
       </div>
     </div>
